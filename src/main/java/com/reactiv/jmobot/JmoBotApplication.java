10ef2604c3d5becd61141ce47621947a9d9e650b
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2025-11-21 21:40:44 +0000

    initialisation du chat bot

---
+++ src/main/java/com/reactiv/jmobot/JmoBotApplication.java
@@ -0,0 +1,13 @@
+package com.reactiv.jmobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmoBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reactiv/jmobot/JmoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiv.jmobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
